2d9a20e5e8e7aa6d7bbb36ce65542fdc8fdacae0
Lukin6as <lucas.rezende06@outlook.com>
2024-05-06 22:40:33 -0300

    Adicionado temperatura max e min e vel do vento

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,10 +2,14 @@ let chave = "e03d5b5937c8b1cfd9ab4c7257e84d99"
 
 function colocarNaTela(dados) {
     console.log(dados)
-    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
-    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
-    document.querySelector(".clima").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon +".png"
-    document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%"
+    document.querySelector("#cidade").innerHTML = "Tempo em: " + dados.name
+    document.querySelector("#temp").innerHTML = "Temperatura: " + Math.floor(dados.main.temp) + "°C"
+    document.querySelector("#temp-max").innerHTML = "Temperatura max: " + Math.floor(dados.main.temp_max) + "°C"
+    document.querySelector("#temp-min").innerHTML = "Temperatura min: " + Math.floor(dados.main.temp_min) + "°C"
+    document.querySelector("#clima").innerHTML = "Clima: " + dados.weather[0].description
+    document.querySelector("#clima-img").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon +".png"
+    document.querySelector("#umidade").innerHTML = "Umidade: " + dados.main.humidity + "%"
+    document.querySelector("#velVento").innerHTML = "Velocidade vento: " + dados.wind.speed + "m/s"
 }
 
 async function buscarCidade(cidade) {
